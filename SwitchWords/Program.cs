a6d6f1d797c0a5de81ae0f1b01c2b2589f2c5b47
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-11-24 11:10:13 +0100

    first commit

---
+++ SwitchWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SwitchWords
+   {
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Rotatetext");
+            Console.WriteLine("2. Change word");
+
+            string userInput = Console.ReadLine();
+
+
+            if (userInput == "1")
+                Console.WriteLine("Write the word you want to rotate");
+            else if (userInput == "2")
+                Console.WriteLine("Write the word you want to change");
+
+        }
+    }
+   }
